1e1682f3170ca1d8b3f210191c3d405348b304d6
Oleksiy Holubyev <lyosha@gmail.com>
2020-09-22 17:35:07 +0200

    As a test of Currency Layer, fetch the base currency using our access token

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'money/bank/currencylayer_bank'
+require 'dotenv/load'
 
 # Listen on all interfaces in the development environment
 # This is needed when we run from Cloud 9 environment
@@ -7,5 +9,12 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
-    erb :currency_selector, layout: :layout_main
+
+    mclb = Money::Bank::CurrencylayerBank.new
+    mclb.access_key = ENV['ACCESS_KEY']
+    mclb.update_rates
+
+    base_currency = mclb.source
+
+    erb :currency_selector, layout: :layout_main, locals: { base_currency: base_currency }
 end
