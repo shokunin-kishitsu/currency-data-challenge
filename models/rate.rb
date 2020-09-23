c78e3506006ce50fe93b67199e096341ce788c5b
Oleksiy Holubyev <lyosha@gmail.com>
2020-09-23 10:53:24 +0200

    Moved some dependencies to Rate model and added a placeholder get_current class method

--- models/rate.rb
+++ models/rate.rb
@@ -1,6 +1,13 @@
+require 'money/bank/currencylayer_bank'
+require 'dotenv/load'
+
 class Rate < ActiveRecord::Base
   validates_presence_of :date
   validates_presence_of :rate
   validates_presence_of :currency_from
   validates_presence_of :currency_to
+
+  def self.get_current(currency_from, currency_to)
+    0.456
+  end
 end
