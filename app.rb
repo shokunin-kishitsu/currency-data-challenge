d9f262e4d0a8c7837d6f59ab6bacf564153d2979
Oleksiy Holubyev <lyosha@gmail.com>
2020-09-23 10:52:07 +0200

    Added placeholders for current rates

--- app.rb
+++ app.rb
@@ -1,18 +1,24 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-require 'money/bank/currencylayer_bank'
-require 'dotenv/load'
+require_relative 'models/rate'
 
 set :bind, '0.0.0.0'
 set :port, 8080
 
 get '/' do
 
-    mclb = Money::Bank::CurrencylayerBank.new
-    mclb.access_key = ENV['ACCESS_KEY']
-    mclb.update_rates
+    #mclb = Money::Bank::CurrencylayerBank.new
+    #mclb.access_key = ENV['ACCESS_KEY']
+    #mclb.update_rates
 
-    base_currency = mclb.source
+    #base_currency = mclb.source
+    #current_rate = Rate.get_current('EUR', 'USD')
+    current_rates = {
+      ['EUR', 'USD'] => 0.45,
+      ['USD', 'EUR'] => 0.46,
+      ['EUR', 'CHF'] => 0.47,
+      ['CHF', 'EUR'] => 0.48,
+    }
 
-    erb :currency_selector, layout: :layout_main, locals: { base_currency: base_currency }
+    erb :currency_selector, layout: :layout_main, locals: { current_rates: current_rates }
 end
